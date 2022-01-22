6dc49dde582d6804731b18b8421b3f924b2a44d2
sc2178@cam.ac.uk <sc2178@cam.ac.uk>
2022-01-22 14:08:25 +0000

    test update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,24 +7,28 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
+    <Seo title="Home" />
+    <h1>Profile: Peter</h1>
+    <p>Name: Peter Lee</p>
+    <p>Age: 25</p>
+    <p>Gender: Male</p>
+    <p>Criminal Records: Nil<StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    /></p>
+    
+    
+    
   </Layout>
 )
 
