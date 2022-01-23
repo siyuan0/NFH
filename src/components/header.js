08f2bbc89b0ac454210764f2578111cc96dbdc08
Daryl Lee Yixiang <daryllee98@gmail.com>
2022-01-23 00:32:08 +0000

    changed some aes

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { StaticImage } from "gatsby-plugin-image"
 
 import * as headerStyles from './header.module.css'
 
 const Header = () => {
 	return (
 		<header className={headerStyles.header}>
+			<div style={{display: `inline-flex`}}>
 			<h1>
 				<Link className={headerStyles.title} to='/'>
 					Non-Fungible Humans
-				</Link>
+				</Link> &nbsp;&nbsp;
+				<nav> 
+					<ul className={headerStyles.navList}>
+						<li>
+								<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Search</Link>
+						</li>
+						<li>
+								<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/upload'>Upload</Link>
+						</li>
+					</ul>
+				</nav>
 			</h1>
-			<nav>
-				<ul className={headerStyles.navList}>
-					<li>
-							<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Search</Link>
-					</li>
-					<li>
-							<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/upload'>Upload</Link>
-					</li>
-				</ul>
-			</nav>
+			</div>
+			<div style={{display: `inline`, alignItems: `center`}}>
+				<StaticImage
+						src="../images/logo1.png"
+						width={100}
+						quality={90}
+						alt="logo"
+				/>
+			</div>
+			
+		
 		</header>
 	)
 }
